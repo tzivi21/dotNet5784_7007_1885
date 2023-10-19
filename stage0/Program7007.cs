105d5912d280f09fa468c581039f8885544cbf50
Tzivi Shachar <tzivish2141@gmail.com>
2023-10-19 21:04:23 +0300

    initial code +welcome functions

---
+++ stage0/Program7007.cs
@@ -0,0 +1,17 @@
+﻿namespace stage0;
+partial  class Program
+{
+    private static void Main(string[] args)
+    {
+        welcome7007();
+        welcome1885();
+        Console.ReadKey();
+    }
+    static partial void welcome1885();
+    private static void welcome7007()
+    {
+        Console.WriteLine("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine($"{name}, welcome to my first console application");
+    }
+}
