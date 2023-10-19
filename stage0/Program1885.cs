48e0c5e56862b9a638b0dfc529a3ef44c3ef3e42
efrat4455z <e0556724455@gmail.com>
2023-10-20 01:16:50 +0300

    Initial Code + Welcome functions (1885)

---
+++ stage0/Program1885.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stage0
+{
+    partial class Program1885
+    {
+        //static partial void Welcome1885()
+        //{
+        //    Console.WriteLine("I am also here!");
+        //}
+    }
+    
+}
